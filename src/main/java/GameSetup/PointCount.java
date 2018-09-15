3dbae7e486b89d1a095321cf9ea704d2c35a300f
Anna Shi <annashi@cmail.carleton.ca>
2018-09-15 16:39:31 -0400

    Test Classes for adding points together.

--- src/main/java/GameSetup/PointCount.java
+++ src/main/java/GameSetup/PointCount.java
@@ -1,4 +1,15 @@
 package GameSetup;
 
+import GameSetup.CardBehaviour.Card;
+
+import java.util.ArrayList;
+
 public class PointCount {
+    public PointCount() {
+        super();
+    }
+
+    public int add(ArrayList<Card> cards) {
+        return -1;
+    }
 }
