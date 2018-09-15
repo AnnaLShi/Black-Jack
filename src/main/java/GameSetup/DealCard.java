2d71bd84a4cb7b2a03887739ac18e0890138eec9
AnnaLShi <31866526+AnnaLShi@users.noreply.github.com>
2018-09-15 14:01:58 -0400

    test added (#17)
    Added tests and classes that would be used develop the dealer (Game Master)

---
+++ src/main/java/GameSetup/DealCard.java
@@ -0,0 +1,51 @@
+package GameSetup;
+
+import CardBehaviour.Card;
+import ReaderTypes.FileReader;
+
+import java.util.ArrayList;
+
+// this should handle the dealing with two cards
+public class DealCard {
+
+    public DealCard() {
+
+    }
+    public DealCard(FileReader file) {
+
+    }
+    public Card hitCalled() {
+        return null;
+    }
+
+    public String standCalled() {
+        return null;
+    }
+
+    public Card getFirstCard() {
+        // get the first card at the top of the arraylist
+        return null;
+    }
+
+    public boolean isFromFile() {
+        return false;
+    }
+
+    public boolean removeFromCardFromDeck(Card card) {
+        return false;
+    }
+
+    public User getUser() {
+        return null;
+    }
+
+    public Dealer getDealer() {
+        return null;
+    }
+
+    public ArrayList<Card> deckStatus() {
+        return null;
+    }
+
+
+}
---
+++ src/main/java/GameSetup/Dealer.java
@@ -0,0 +1,18 @@
+package GameSetup;
+
+import CardBehaviour.Card;
+
+import java.util.ArrayList;
+
+public class Dealer {
+    public Dealer() {
+
+    }
+    public ArrayList<Card> getDealerHand () {
+        return null;
+    }
+
+    public boolean addCard(Card card) {
+        return false;
+    }
+}
