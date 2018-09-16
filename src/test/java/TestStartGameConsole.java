6d247a5409954dfa1672640fabea2becc6d14fc8
AnnaLShi <31866526+AnnaLShi@users.noreply.github.com>
2018-09-16 18:46:36 -0400

    Start screen (#30)
    * New StartScreen
    
    * just making sure everything is updated
    
    * test start game

---
+++ src/test/java/TestStartGameConsole.java
@@ -0,0 +1,36 @@
+import GameSetup.StartGame;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestStartGameConsole {
+
+    @Test
+    @DisplayName("Check for No")
+    public void checkForNoResponse() {
+        StartGame game = new StartGame();
+        assertTrue(game.checkNo("N"));
+        assertTrue(game.checkNo("No"));
+        assertTrue(game.checkNo("NO"));
+        assertFalse(game.checkNo("ksdal"));
+    }
+
+    @Test
+    @DisplayName("Check for Yes")
+    public void checkforYesResponse() {
+        StartGame game = new StartGame();
+        assertTrue(game.checkYes("Yes"));
+        assertTrue(game.checkYes("y"));
+        assertFalse(game.checkYes("7894"));
+    }
+
+
+    @Test
+    @DisplayName("Check for invalid input")
+    public void checkForInvaildResponse(){
+        StartGame game = new StartGame();
+        assertTrue(game.invalidInput("asdfsaf"));
+    }
+}
