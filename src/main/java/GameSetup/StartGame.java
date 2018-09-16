9a53f301359aa1fc7aef5e3b49a77c2db73f6f70
Anna Shi <annashi@cmail.carleton.ca>
2018-09-16 18:45:22 -0400

    test start game

---
+++ src/main/java/GameSetup/StartGame.java
@@ -0,0 +1,19 @@
+package GameSetup;
+
+public class StartGame {
+
+    public void run() {
+
+    }
+
+    public boolean checkNo(String in){
+        return false;
+    }
+    public boolean checkYes(String in) {
+        return false;
+    }
+
+    public boolean invalidInput(String in) {
+        return false;
+    }
+}
