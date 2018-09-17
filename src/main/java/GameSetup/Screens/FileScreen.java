01dedd16e4da5d3b697da38bdc62f472c6744173
Anna Shi <annashi@cmail.carleton.ca>
2018-09-16 20:39:54 -0400

    bring Test files

--- src/main/java/GameSetup/Screens/FileScreen.java
+++ src/main/java/GameSetup/Screens/FileScreen.java
@@ -1,4 +1,16 @@
 package GameSetup.Screens;
 
 public class FileScreen {
+
+    public boolean isInvalid(String in) {
+        return false;
+    }
+
+    public boolean isValid(String in) {
+        return false;
+    }
+
+    public boolean isBack(String in) {
+        return false;
+    }
 }
